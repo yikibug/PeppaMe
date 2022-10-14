f9bcd137b24392ed74d24fcaaca95b180001db78
cgx <metrointerface@live.com>
2022-10-14 11:07:53 +0800

    修复系统bug

---
+++ Example/Pods/Target Support Files/PeppaMe/PeppaMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PeppaMe : NSObject
+@end
+@implementation PodsDummy_PeppaMe
+@end
---
+++ Example/Pods/Target Support Files/Pods-PeppaMe_Example/Pods-PeppaMe_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PeppaMe_Example : NSObject
+@end
+@implementation PodsDummy_Pods_PeppaMe_Example
+@end
